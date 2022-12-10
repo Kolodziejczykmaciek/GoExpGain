bf1aaf68b41c006f108f17d8e28099cf85a6dc4c
Your Name <maciej.kolodziejczyk@airhelp.com>
2022-12-11 00:48:58 +0100

    type func in struct

--- typefunc/go.mod
+++ typefunc/go.mod
@@ -1,3 +1,5 @@
 module GoExpGain/typefunc
 
 go 1.19
+
+require k8s.io/utils v0.0.0-20221128185143-99ec85e7a448
--- typefunc/main.go
+++ typefunc/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+)
+
+type hashFunc func (string) string  
+
+type Machine struct {
+	nameHashFunc hashFunc
+}
+
+func (m *Machine) runJob(name string) error {
+	newhash := m.nameHashFunc(name)
+	fmt.Printf("String \"%s\" (sha256): %s", name, newhash)
+	
+	return nil
+}
+
+func hashString(s string) string {
+	hash := sha256.Sum256([]byte(s))
+	hashToString := hex.EncodeToString(hash[:])
+
+	return hashToString
+}
 
 func main() {
 	fmt.Println("Hello")
+	m := &Machine{
+		nameHashFunc: hashString,
+	}
+	m.runJob("Ignacy")
 }
